6a54640bd1e55254106d5fb07ed2f3b940bd2075
lisachandra <>
2023-05-15 22:31:04 +0700

    update packages

---
+++ src/init.lua
@@ -0,0 +1,5 @@
+--loadstring(game:HttpGet("https://github.com/zxibs/Revo/raw/main/src/init.lua"))()
+
+local Packager: Packager = loadstring(game:HttpGet("https://gist.github.com/zxibs/f1a148a72a058636296c0bc991aca130/raw/8c2fb87e5d9701bbad22916462480ed528d6f6ef/Packager.lua"))()
+
+
