dc0c6c2edc0b5b171d463b790cff722118f0ae04
lisachandra <>
2023-05-23 18:31:44 +0700

    fix tab

--- src/Page.lua
+++ src/Page.lua
@@ -77,8 +77,6 @@ local function Page(props: props & internal, hooks: RoactHooks.Hooks)
                 Text = props.name,
 
                 [Roact.Event.MouseButton1Click] = function(_self: TextButton)
-                    print("testing")
-
                     if not props.opened then
                         props.open()
                     end
