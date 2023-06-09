469487139ad5cb6e063e8649a81b348f5ec2b236
lisachandra <>
2023-06-09 16:14:02 +0700

    page debug

--- src/Page.lua
+++ src/Page.lua
@@ -31,6 +31,8 @@ local function Page(props: internal, hooks: RoactHooks.Hooks)
             local elementLocations = hooks.useValue({} :: Dictionary<string>)
         
             local elements = {}; for elementName, element in pairs((props :: any)[Roact.Children]) do
+                print(elementName)
+
                 elementLocations.value[elementName] = elementLocations.value[elementName] or `/{HttpService:GenerateGUID()}_tip`
                 element.props = merge(element.props, {
                     info = merge(element.props.info, {
